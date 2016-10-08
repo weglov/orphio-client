544727df6224e8918118ed2ed6b80a5b680ab31c
weglov <viktor@scheglov.pro>
2016-10-08 23:21:55 +0300

    add login

--- src/components/panel/panel.js
+++ src/components/panel/panel.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import MistakePage from './m';
 import { browserHistory } from 'react-router';
 import { load } from '../../actions/Api';
+import { connect } from 'react-redux'
+import  { login, logout } from '../../actions'
 const token = window.localStorage.getItem('o__token');
 
 class Panel extends Component {
   static onEnter(nextState, replace) {
     var token = window.localStorage.getItem('o__token');
     var id = window.localStorage.getItem('o__id');
+    var user = {
+      id:  window.localStorage.getItem('o__id'),
+      token: window.localStorage.getItem('o__token'),
+      email: window.localStorage.getItem('o__email')
+    }
+    this.props.login(user)
     if (token) {
       load('users/' + id, token)
         .then((m) => {
@@ -38,4 +46,10 @@ class Panel extends Component {
   }
 };
 
-export default Panel;
+function mapStateToProps(state) {
+  return { index: state.login }
+}
+
+
+
+export default connect(mapStateToProps, {login, logout})(Panel)
